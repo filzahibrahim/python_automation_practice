ddf6625cede02b49c330fff0948375eb04962465
filzahibrahim <filzahibrahim147@gmail.com>
2026-06-24 00:35:24 +0500

    Learn Variables in Chapter01

---
+++ chapter01-python-basics/variables.py
@@ -0,0 +1,8 @@
+# name = (variable) : think of it like a box titled name
+# "filzah" is a text (string type) : think of this like a note inside the box
+name = "filzah"   
+print(name)  # print the note inside that box 
+age = 20 #variable (integer type)
+print(age)  
+country = "Pakistan"
+print(country)

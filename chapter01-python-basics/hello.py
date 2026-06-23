ab42a6aca5e8c69546e16de23c217aad4261b13e
filzahibrahim <filzahibrahim147@gmail.com>
2026-06-23 23:06:21 +0500

    Chapter01: Print function

---
+++ chapter01-python-basics/hello.py
@@ -0,0 +1,11 @@
+print('Hello World')
+print('I am Filzah')
+print('I am learning Python Automation!')
+print('My favorite game is Sudoku')
+print('My favorite food is Pizza')
+print('I want to become a Software Developer')
+
+# for practice print few things on your own
+
+print('Python is easy and fun')
+print('I need to practice everyday to improve my skills!!!')

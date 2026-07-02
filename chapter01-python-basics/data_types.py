4b914db621ba71572b0b23a048a70b8eb31af996
filzahibrahim <filzahibrahim147@gmail.com>
2026-07-02 18:18:50 +0500

    Chapter01: Data types

---
+++ chapter01-python-basics/data_types.py
@@ -0,0 +1,16 @@
+
+name = "filzah" #string type
+age = 20 #integer type
+isStudent = True #boolean type
+GPA = 3.14 #float type
+
+#practice
+print(type("Hello"))  #guess their types
+print(type(10))
+print(type(3.14))
+print(type(True))
+
+print(2 + 3) #guess the output
+print("2" + "3") #guess
+print("Hello" + " " + "World") #guess
+print(isStudent) #guess

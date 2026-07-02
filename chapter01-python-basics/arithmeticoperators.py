e615d9e3b2699a249991edcf3e7cb86f7b6e3ed4
filzahibrahim <filzahibrahim147@gmail.com>
2026-07-02 21:11:00 +0500

    Chapter01: Arithmetic Operators

---
+++ chapter01-python-basics/arithmeticoperators.py
@@ -0,0 +1,23 @@
+5 + 3 #addition
+"hello " + "world" #concatenation
+15 - 5 #substraction
+3 * 2 #multiplication
+"Hello" * 3 #repetition
+10 / 2 #division (returns float)
+10 // 2 #floor division (returns whole number)
+10 % 2 #modulus (remainder)
+5 ** 2 #exponent 
+
+#follows PEMDADS
+print(2 + 3 * 4) 
+print((2 + 3) * 4) 
+
+#practice
+print(9 + 6 // 2) #guess the output 12
+print(15 % 4) #guess 3
+print(2 ** 5) #guess 64
+print(7 * (3 + 2)) #guess 35
+print(20 / 5 + 3) #guess 7.0
+print("Go" * 4) #guess 
+print(10 // 3) #guess 3
+print(10 % 3) #guess 1
--- chapter01-python-basics/data_types.py
+++ chapter01-python-basics/data_types.py
@@ -1,6 +1,6 @@
 
 name = "filzah" #string type
-age = 20 #integer type
+#age = 20 #integer type
 isStudent = True #boolean type
 GPA = 3.14 #float type
 
@@ -12,5 +12,16 @@ print(type(True))
 
 print(2 + 3) #guess the output
 print("2" + "3") #guess
-print("Hello" + " " + "World") #guess
+print("Hello" + " " + "World") #guess #this is concatenation
 print(isStudent) #guess
+
+age = input('What is your age? ')
+print(age)
+print(type(age)) #guess
+
+#Expressions = anything that holds a value
+2 + 3 #evaluates to 5
+"Hello" #evaluates to "Hello"
+10 #evaluates to 10
+2 > 3 #evaluates to False
+name #evaluates to "filzah"

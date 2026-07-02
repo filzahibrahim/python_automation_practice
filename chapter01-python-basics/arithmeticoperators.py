420fdbd3513b658505a0b3316002962378cae1f1
Filzah <filzahibrahim147@gmail.com>
2026-07-02 21:14:07 +0500

    Chapter01: Arithmetic Operators

--- chapter01-python-basics/arithmeticoperators.py
+++ chapter01-python-basics/arithmeticoperators.py
@@ -13,11 +13,11 @@ print(2 + 3 * 4)
 print((2 + 3) * 4) 
 
 #practice
-print(9 + 6 // 2) #guess the output 12
-print(15 % 4) #guess 3
-print(2 ** 5) #guess 64
-print(7 * (3 + 2)) #guess 35
-print(20 / 5 + 3) #guess 7.0
+print(9 + 6 // 2) #guess the output 
+print(15 % 4) #guess 
+print(2 ** 5) #guess 
+print(7 * (3 + 2)) #guess 
+print(20 / 5 + 3) #guess 
 print("Go" * 4) #guess 
-print(10 // 3) #guess 3
-print(10 % 3) #guess 1
+print(10 // 3) #guess 
+print(10 % 3) #guess 

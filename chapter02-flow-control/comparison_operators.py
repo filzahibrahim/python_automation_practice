3f13df1f4bd81fde0bfe839d51562436b658942d
filzahibrahim <filzahibrahim147@gmail.com>
2026-07-17 18:59:42 +0500

    Chapter02: Comparison Operators

---
+++ chapter02-flow-control/comparison_operators.py
@@ -0,0 +1,23 @@
+#booleans are True and False
+#comparisons produce booleans
+
+#comparison operators 
+5 == 5 #equals to
+5!= 4 #not equals to
+5 > 3 #greater than
+5 < 7 #less than
+18 >= 18 # greater than or equals
+4 <= 5 #less than or equals
+#5 = 5 #WRONG! this is assignment, python expects a variable on the left
+
+#a comparison always ask a question
+#is it true or false?
+
+#practice
+5 == 5 #guess the output
+5 == 3
+10 != 10
+10 != 7
+20 > 15
+8 < 2
+9 <= 12

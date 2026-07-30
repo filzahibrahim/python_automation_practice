fc6773d3bae3bd242b19fccfcd91cf81d4b5da47
filzahibrahim <filzahibrahim147@gmail.com>
2026-07-30 20:40:16 +0500

    Chapter02: While Loops

--- chapter02-flow-control/while_loops.py
+++ chapter02-flow-control/while_loops.py
@@ -14,8 +14,8 @@ while count <= 5: #the loop continues as long as the condition is true
     print(count) #do this while the is true
     count = count + 1 #incrementing by 1 to move to the next number. (we can increment or decrement by any number)
 
-#if we dont increment by 1, count will never increase remaining 1 forever which causes the condition
-#to never become false causing an infinite loop which can crash a program
+#if we don't increment by 1, count will never increase remaining 1 forever which causes the condition
+#to never become false causing an infinite loop which can crash a program.
 # we need something that eventually make the condition of the loop false.
 #when the condition becomes false we exit the loop and continue the program.
 
@@ -24,3 +24,6 @@ count = 5
 while count >= 1:
     print(count)
     count = count - 1
+
+#this is out of the loop
+print("Program end")

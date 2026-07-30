816f4551593ce54c2748453392b7dfa20fe4a87c
filzahibrahim <filzahibrahim147@gmail.com>
2026-07-30 21:25:45 +0500

    Chapter02: Break & Continue

---
+++ chapter02-flow-control/break_&_continue.py
@@ -0,0 +1,31 @@
+count = 1
+
+while count <= 10: # the loop works normally when the break condition is false
+    print(count)
+
+    if count == 5:
+        break  #it means to STOP the loop immediately when the if condition becomes true
+    #break ends the loop and exits it when the condition becomes true. 
+
+    count = count + 1
+
+count = 0
+while count < 5:
+    count = count + 1 
+
+    if count == 3:
+        continue #it means to skip THIS iteration and go back to the top of the loop
+    #it basically means to skip wtvr comes after it and then continue the loop 
+
+    print(count)  #so due to continue, this part is skipped and 3 doesnt get printed and we continue the loop
+    #run this progranm to understand it better
+
+# practice - guess the output
+count = 11
+while count > 1:
+    count = count - 1
+
+    if count < 8:
+        break
+
+    print(count)

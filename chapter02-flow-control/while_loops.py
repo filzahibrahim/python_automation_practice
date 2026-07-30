4d9558ff008ad1c4d2c9d42656de169be26f1a47
filzahibrahim <filzahibrahim147@gmail.com>
2026-07-30 20:28:33 +0500

    Chapter02: While loops

---
+++ chapter02-flow-control/while_loops.py
@@ -0,0 +1,26 @@
+#instead of printing everything one by one
+
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+
+#we use while loops. For example:
+
+count = 1
+
+while count <= 5: #the loop continues as long as the condition is true
+    print(count) #do this while the is true
+    count = count + 1 #incrementing by 1 to move to the next number. (we can increment or decrement by any number)
+
+#if we dont increment by 1, count will never increase remaining 1 forever which causes the condition
+#to never become false causing an infinite loop which can crash a program
+# we need something that eventually make the condition of the loop false.
+#when the condition becomes false we exit the loop and continue the program.
+
+#we can also print it in descending order
+count = 5
+while count >= 1:
+    print(count)
+    count = count - 1

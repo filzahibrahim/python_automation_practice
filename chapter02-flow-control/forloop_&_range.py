3fa6a165f0cab84b22130a4f9ca4df054acd802b
filzahibrahim <filzahibrahim147@gmail.com>
2026-08-03 21:15:26 +0500

    Chapter02: For Loop

---
+++ chapter02-flow-control/forloop_&_range.py
@@ -0,0 +1,22 @@
+#unlike while loop which keeps running while a condition is true
+#for loop goes through each item one by one
+
+for fruit in ["apple", "mango", "banana"]: 
+    print(fruit)
+
+#fruit is a variable
+#for loop goes through every item one by one as the loop goes on
+#first the value of fruit is "apple" which gets printed, then in the second loop the value becomes "mango" and gets printed,
+#and in the last loop the value of fruit becomes "banana" which gets printed. run to seee the output
+
+for color in ["purple", "red", "blue"]:
+    print(color) #guess the output
+
+#with a string
+for letter in "Python": #python goes through each letter one by one 
+    print(letter) #and then print each letter one by one
+
+word = "Python"
+
+for letter in word:
+    print("I found:", letter) #guess the output

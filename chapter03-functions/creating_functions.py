23a2e6fbf2fcd7a99e956b291e32729ae91c81fd
filzahibrahim <filzahibrahim147@gmail.com>
2026-08-05 23:54:40 +0500

    Chapter03: Calling Functions

---
+++ chapter03-functions/creating_functions.py
@@ -0,0 +1,24 @@
+#functions are a reusable block of code that performs a specific task
+#we have used functions before like
+int()
+float()
+input()
+bool()
+str()
+type()
+print()
+#they are functions that perform a specific task
+
+#we can create our own functions
+#instead of writing something again and again. we store it in a function and call it whenever needed
+
+#here i am defining a function called greet(). Now whenever i will call this function it will execute the 
+#code inside. Remember this is just defining the function so it is not run by python unless i call it.
+def greet(): #def stands for define
+    print("Hello!! Welcome to my program")
+    print("Here i share everything I learn")
+
+greet() #this is "calling" the function. and if i run it. it will execute the code that is inside the function.
+
+#defining the function is like writing a recipe inside your notes
+#and calling the function is like giving python that recipe to cook.

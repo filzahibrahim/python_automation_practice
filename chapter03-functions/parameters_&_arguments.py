90fb117ee1c89f3ba78cf4e533217ef2f899a35b
filzahibrahim <filzahibrahim147@gmail.com>
2026-08-11 15:11:34 +0500

    Chapter03: Multiple Parameters & Missing Arguments

--- chapter03-functions/parameters_&_arguments.py
+++ chapter03-functions/parameters_&_arguments.py
@@ -9,3 +9,16 @@ greet("Azusa") #Here Azusa is an "argument". This is the value of the variable/p
 
 greet("Emma")
 greet("Sarah") #same function, diff values
+
+#we can also have multiple parameters
+
+def introduce(name, age): #we can ask for more than one value
+    print(f"My name is {name} and I am {age} years old")
+
+introduce("Azusa", 20) #when calling the function. the argument should be in the same order as the parameter
+#if you write 20 first. Python will think that "name" is an int and store 20 in it. and store "Azusa" inside the 
+#parameter "age". This is why order matters.
+
+#introduce("Azusa") 
+#this will give an error because python expects the same number of arguments as parameters.
+#when we call a function, we have to provide all the required information for its parameter.

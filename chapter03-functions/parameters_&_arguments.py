2916d656b9e8c770224c925945cb87d5419fdc04
filzahibrahim <filzahibrahim147@gmail.com>
2026-08-11 16:55:40 +0500

    Chapter03: Default Parameters

--- chapter03-functions/parameters_&_arguments.py
+++ chapter03-functions/parameters_&_arguments.py
@@ -15,6 +15,7 @@ greet("Sarah") #same function, diff values
 def introduce(name, age): #we can ask for more than one value
     print(f"My name is {name} and I am {age} years old")
 
+#python doesnt know that name should be a string and age should be an int. It just matches the order of the parameters
 introduce("Azusa", 20) #when calling the function. the argument should be in the same order as the parameter
 #if you write 20 first. Python will think that "name" is an int and store 20 in it. and store "Azusa" inside the 
 #parameter "age". This is why order matters.
@@ -22,3 +23,27 @@ introduce("Azusa", 20) #when calling the function. the argument should be in the
 #introduce("Azusa") 
 #this will give an error because python expects the same number of arguments as parameters.
 #when we call a function, we have to provide all the required information for its parameter.
+
+#we can have also have parameters with an default value
+
+def introduce(name, age = 18): #if we dont give age a value python will just print 18 instead
+    print(f"My name is {name} and I am {age} years old")
+
+#so now this won't give an error
+introduce("Azusa") #we didnt give a value for age when calling the function
+#but there is alr a default value for age so it won't throw an error
+
+#we can also give age a value even if there is alr a default value
+introduce("Azusa", 20) #python just assigns age the new value we gave in our argument
+
+#default value works as a backup. if we dont give a value in our argument. Python uses the default value but if
+#we do give a value in our argument. Python assigns that value to the parameter age and prints it.
+
+#another example
+
+def greet(name, message = "Hello!"):
+    print(f"{message}, {name}")
+
+greet("Azusa")
+greet("Sarah", "Hiiii!")
+greet("Emma", "Morning!")

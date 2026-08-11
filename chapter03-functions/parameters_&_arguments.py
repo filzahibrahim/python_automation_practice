08c453a1b42255c4d816d4dc221a7c7be0a981e7
filzahibrahim <filzahibrahim147@gmail.com>
2026-08-11 14:53:05 +0500

    Chapter03: Parameters & Arguments

---
+++ chapter03-functions/parameters_&_arguments.py
@@ -0,0 +1,11 @@
+#we give our function a variable that can recieve information
+def greet(name): #name is a variable inside function waiting to receice a value. Also called "parameter"
+    print(f"Hello! {name}") 
+
+greet("Azusa") #Here Azusa is an "argument". This is the value of the variable/parameter 
+#we write the value(argument) of the variable(parameter) when calling the function
+
+#parameter - argument lets us have diff value using the same function multiple times
+
+greet("Emma")
+greet("Sarah") #same function, diff values

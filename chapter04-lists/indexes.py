d6ae84ffec2aa0b9b167f520ba14e4284a93c9d6
filzahibrahim <filzahibrahim147@gmail.com>
2026-09-01 20:00:33 +0500

    Chapter04: Creating Lists & Indexes

---
+++ chapter04-lists/indexes.py
@@ -0,0 +1,21 @@
+#what if i want to store multiple values 
+fruit1 = "apple"
+fruit2 = "mango"
+fruit3 = "banana"
+
+#we can use lists for that
+
+fruits = ["apple", "mango", "banana"]
+
+#in list, each item has an index
+#indexes are 0, 1, 2..
+#so apple is at 0 index, mango at 1, banana at 2
+
+#we can access an item in a list by using its index with the list variable
+print(fruits[0]) #will print whatever is on index 0
+
+#we can also use negative index
+# -1 means the last item in the list
+
+print(fruits[-1]) #will print whatever is the last item in the list
+print(fruits[-2]) #will print the second last item

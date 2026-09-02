4c7638ebe64efa3e3904a52b614ae44136dd26c1
filzahibrahim <filzahibrahim147@gmail.com>
2026-09-03 02:31:45 +0500

    Chapter04: len()

---
+++ chapter04-lists/len().py
@@ -0,0 +1,10 @@
+#len() returns the number of items in an object
+#in string, len() counts character, punctuations and spaces
+
+spam = "yummy"
+print(len(spam)) #yummy has 5 characters so it returns 5
+
+#in lists, it returns the number of items in it
+
+eggs = ['yeast', 'fish', 'milk', 'bread']
+print(len(eggs)) #the list has 4 items so it returns 4
